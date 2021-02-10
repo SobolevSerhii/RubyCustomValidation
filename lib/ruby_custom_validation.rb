1171108053d3ad79717509f6909ccf390fb4aa79
Sergio9999 <sobolevsergeysom@gmail.com>
2021-02-10 12:29:06 +0200

    Start to development ruby_custom_validation. First steps

--- lib/ruby_custom_validation.rb
+++ lib/ruby_custom_validation.rb
@@ -2,7 +2,19 @@
 
 require_relative "ruby_custom_validation/version"
 
+# module for implement custom validation for Rails models
 module RubyCustomValidation
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.validate(attribute_name, presence: nil, type: nil, format: nil)
+    if presence.nil? && type.nil? && format.nil?
+      raise ArgumentError, "validate must be has attibutes such as :presence, :type, :format"
+    end
+
+    new(attribute_name, presence: presence, type: type, format: format)
+  end
+
+  def self.new(attribute_name, presence: nil, type: nil, format: nil)
+    binding.pry
+  end
 end
--- ruby_custom_validation.gemspec
+++ ruby_custom_validation.gemspec
@@ -30,5 +30,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
+  spec.add_development_dependency "pry"
+  spec.add_development_dependency "rubocop"
+  spec.add_development_dependency "rufo"
+  spec.add_development_dependency "solargraph"
 end
